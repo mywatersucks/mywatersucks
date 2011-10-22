5ab1c3eb22dae9f3bb31d7fe0fa0c1806951aa2b
Yasith Vidanaarachchi <yasith.vidanaarachchi@utoronto.ca>
2011-10-22 15:33:14 -0400

    Added scripts to recreate the tables in the database.

---
+++ sql/create_tables.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+  `address_parts` varchar(200) DEFAULT NULL,
+  `longitude` varchar(10) DEFAULT NULL,
+  `latitude` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`location_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `reports`;
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  `sms_contents` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `targets`;
+CREATE TABLE `targets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(20) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `twitter` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `jurisdiction` varchar(50) DEFAULT NULL,
+  `position` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(20) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `uname` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
